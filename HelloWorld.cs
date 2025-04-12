6f5a08bf569185113473059c618fe0cbadc03575
Dima583art <d89545858@gmail.com>
2025-04-12 20:11:50 +0300

    Scripts

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //вывод консоль
+      print("HelloWorld") ; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
